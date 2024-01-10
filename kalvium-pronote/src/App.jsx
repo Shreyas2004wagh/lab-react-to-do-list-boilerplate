c22b1826c62f84d8e649dca85b821a47aa2a6e0c
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-01-10 15:03:54 +0530

    done

---
+++ kalvium-pronote/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Todoapp from "./components/Todoapp"
+import React, { Component } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <Todoapp />
+      
+    </>
+  )
+}
+
+export default App
